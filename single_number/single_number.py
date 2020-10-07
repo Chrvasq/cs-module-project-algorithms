0dd2f2305690c536b26f2fffb676528f118e2746
Chris Vasquez <chrvasq@outlook.com>
2020-10-06 20:10:18 -0700

    Complete single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,17 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    nums = {}
 
-    pass
+    for num in arr:
+        if num in nums:
+            nums[num] += 1
+        else:
+            nums[num] = 1
+
+    for key, value in nums.items():
+        if value is 1:
+            return key
 
 
 if __name__ == '__main__':
